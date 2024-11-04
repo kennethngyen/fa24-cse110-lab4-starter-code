758f8f917d3dafb17cc375444edf253c4caad60a
kennethngyen <kennyboy860@gmail.com>
2024-11-04 14:29:54 -0800

    lab6 done

--- server/src/expenses/expense-utils.ts
+++ server/src/expenses/expense-utils.ts
@@ -1,37 +1,48 @@
+import { Database } from "sqlite";
 import { Expense } from "../types";
 import { Request, Response } from "express";
 
-export function createExpenseServer(req: Request, res: Response, expenses: Expense[]) {
+export async function createExpenseServer(req: Request, res: Response, db: Database) {
     const { id, cost, description } = req.body;
-
+ 
     if (!description || !id || !cost) {
         return res.status(400).send({ error: "Missing required fields" });
     }
-
-    const newExpense: Expense = {
-        id: id,
-        description,
-        cost,
+ 
+    try {
+        await db.run('INSERT INTO expenses (id, description, cost) VALUES (?, ?, ?);', [id, description, cost]);
+    } catch (error) {
+        return res.status(400).send({ error: `Expense could not be created, + ${error}` });
     };
+ 
+    res.status(201).send({ id, description, cost });
+} 
 
-    expenses.push(newExpense);
-    res.status(201).send(newExpense);
-}
-
-export function deleteExpense(req: Request, res: Response, expenses: Expense[]) {
+export async function deleteExpense(req: Request, res: Response, db: Database) {
     const { id } = req.params;
 
-    const index = expenses.findIndex(expense => expense.id === id);
-
-    if (index === -1) {
-        return res.status(404).send({ error: "Expense not found" });
+    if (!id) {
+        return res.status(400).send({ error: "Needs ID in request" });
     }
 
-    expenses = expenses.filter((e) =>e.id!=id);
-    // console.log("deleted", expenses);
-    res.status(200).send({"newExpense":expenses});
+    try {
+        const result = await db.run('DELETE FROM expenses WHERE id = ?;', [id]);
+
+        if (result.changes === 0) {
+            return res.status(400).send({ error: 'Expense failed create' });
+        }
+
+        res.status(200).send({ message: "Expense deleted successfully" });
+    } catch (error) {
+        return res.status(500).send({ error: `Could not delete expense: ${error}` });
+    }
 }
 
-export function getExpenses(req: Request, res: Response, expenses: Expense[]) {
-    res.status(200).send({ "data": expenses });
+export async function getExpenses(req: Request, res: Response, db: Database) {
+    try {
+        const expenses = await db.all('SELECT * FROM expenses;');
+        res.status(200).send({ data: expenses });
+    } catch (error) {
+        res.status(500).send({ error: `Failed to get all expenses with error: ${error}` });
+    }
 }
