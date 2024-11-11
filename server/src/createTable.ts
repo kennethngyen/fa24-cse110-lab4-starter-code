9d41b1c8d2238496b72992346899b0746a22ed21
Bella Jeong <lahrry0925@gmail.com>
2024-11-11 07:07:00 -0800

    lab 6 done fixed little error in index.ts

---
+++ server/src/createTable.ts
@@ -0,0 +1,21 @@
+import { open } from "sqlite";
+import sqlite3 from "sqlite3";
+
+const initDB = async () => {
+ // Open the database connection
+ const db = await open({
+   filename: "database.sqlite",
+   driver: sqlite3.Database,
+ });
+ // Create a "budget" table if it doesn't exist
+ await db.exec(`
+   CREATE TABLE IF NOT EXISTS expenses (
+     id TEXT PRIMARY KEY,
+     description TEXT NOT NULL,
+     cost INTEGER NOT NULL
+   );
+ `);
+ return db;
+};
+
+export default initDB;
--- server/src/expenses/expense-utils.ts
+++ server/src/expenses/expense-utils.ts
@@ -1,37 +1,63 @@
-import { Expense } from "../types";
 import { Request, Response } from "express";
+import { Database } from "sqlite";
 
-export function createExpenseServer(req: Request, res: Response, expenses: Expense[]) {
+export async function createExpenseServer(req: Request, res: Response, db: Database) {
     const { id, cost, description } = req.body;
-
+ 
     if (!description || !id || !cost) {
         return res.status(400).send({ error: "Missing required fields" });
     }
-
-    const newExpense: Expense = {
-        id: id,
-        description,
-        cost,
+ 
+    try {
+        await db.run('INSERT INTO expenses (id, description, cost) VALUES (?, ?, ?);', [id, description, cost]);
+    } catch (error) {
+        return res.status(400).send({ error: `Expense could not be created, + ${error}` });
     };
+ 
+    res.status(201).send({ id, description, cost });
+ 
+ 
+ }
 
-    expenses.push(newExpense);
-    res.status(201).send(newExpense);
-}
-
-export function deleteExpense(req: Request, res: Response, expenses: Expense[]) {
-    const { id } = req.params;
-
-    const index = expenses.findIndex(expense => expense.id === id);
-
-    if (index === -1) {
-        return res.status(404).send({ error: "Expense not found" });
+export async function getExpenses(req: Request, res: Response, db: Database){
+    try{
+        const expenses = await db.all('SELECT * FROM expenses;');
+        res.status(200).send({data: expenses});
+    } catch (error){
+        res.status(500).send({error: `Failed to get all expenses, + ${error}`});
     }
-    console.log(id)
-    expenses.splice(index, 1);
-    console.log("deleted", expenses);
-    res.status(200).send({"newExpense":expenses});
-}
+ }
 
-export function getExpenses(req: Request, res: Response, expenses: Expense[]) {
-    res.status(200).send({ "data": expenses });
+export async function deleteExpense(req: Request, res:Response, db: Database){
+    const{id} = req.params;
+    if(!id){
+        return res.status(400).send({error: 'No ID found!'});
+    }
+
+    try{
+        const result = await db.run('DELETE FROM expenses WHERE id = ?;', [id]);
+        if (result.changes === 0){
+            return res.status(400).send({error: 'Failed to create expenses'});
+        }
+        res.status(200).send({message: 'Expense successfully got deleted!'});
+    } catch (error){
+        return res.status(500).send({error: `Failed to delete expense: ${error}`});
+    }
 }
+// export function deleteExpense(req: Request, res: Response, expenses: Expense[]) {
+//     const { id } = req.params;
+
+//     const index = expenses.findIndex(expense => expense.id === id);
+
+//     if (index === -1) {
+//         return res.status(404).send({ error: "Expense not found" });
+//     }
+//     console.log(id)
+//     expenses.splice(index, 1);
+//     console.log("deleted", expenses);
+//     res.status(200).send({"newExpense":expenses});
+// }
+
+// export function getExpenses(req: Request, res: Response, expenses: Expense[]) {
+//     res.status(200).send({ "data": expenses });
+// }
